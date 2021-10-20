215e5eb1a47df11abf6b6810033cef88f31ca3cf
ivanschuhupf <91157004+ivanschuhupf@users.noreply.github.com>
2021-10-20 16:24:43 -0300

    ivan

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
-<?php
-session_start();
-if(!isset($_SESSION["nome_usuario"])){
-    echo "<script>alert('Você não está Logado!');location.href=\"index.php\";</script>";
-}else {
-    require_once'app/view/paginaPrincipal.php';
-}
-?>
-=======
 <!DOCTYPE html>
 <html lang="pt-br">
-    <title>Universidade de Passo Fundo</title>
-    <?php
-        require_once 'app/view/paginaPrincipal.php';
-    ?>
+<title>Universidade de Passo Fundo</title>
+<?php
+session_start();
+if (!isset($_SESSION["nome_usuario"])) {
+    echo "<script>alert('Você não está Logado!');location.href=\"index.php\";</script>";
+} else {
+    require_once 'app/view/paginaPrincipal.php';
+}
+?>
+
 </html>
->>>>>>> 81c2dae0d8c1fc1f7e56d256b5916bd8acab6c2e
